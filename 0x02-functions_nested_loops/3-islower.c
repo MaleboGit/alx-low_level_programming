c78cf2371560ec49d3602671533699c9e6ee5f41
MaleboGit <maleboinfo@gmail.com>
2023-06-20 15:46:39 -0700

    function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 void _putchar(char x);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
