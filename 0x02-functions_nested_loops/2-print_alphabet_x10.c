8b80e5615609d959247184259c0ad5e3cc0485fe
MaleboGit <maleboinfo@gmail.com>
2023-06-20 15:01:55 -0700

    function that prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the lowercase alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		void print_alphabet(void);
+		count++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 void _putchar(char x);
 void print_alphabet(void);
+void print_alphabet_x10(void);
