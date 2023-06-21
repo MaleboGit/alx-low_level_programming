ffdcdabe015a9bdb3c41a870ef7718425a06951d
MaleboGit <maleboinfo@gmail.com>
2023-06-21 12:30:06 -0700

    function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @num1: Integer parameter
+ * @num2: Integer parameter
+ *
+ * Return:
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+	return (sum);
+}
