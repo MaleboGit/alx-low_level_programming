097490ce3ed5c3a6ef9c5ca9e4324ecaa4d20ef5
MaleboGit <maleboinfo@gmail.com>
2023-07-02 12:14:21 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: Integer parameter
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
