bc0168258f91203615f6e9ad7d6ddba424f2b6d2
MaleboGit <maleboinfo@gmail.com>
2023-07-03 12:04:06 -0700

    function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: character parameter to compare
+ * @s2: second char parameter to compare
+ *
+ * Return: strcmp(s1, s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
