24bee54a8c29e53d75de10527ba01739942c7ad8
MaleboGit <maleboinfo@gmail.com>
2023-07-02 23:50:22 -0700

    function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string
+ *
+ * @dest: character parameter pointing to a string parameter
+ * @src: character parameter pointing to a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
