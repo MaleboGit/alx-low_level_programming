732d6f8e7ee2575c2dacb52099ddc50f51ad928e
MaleboGit <maleboinfo@gmail.com>
2023-07-03 11:33:54 -0700

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: charracter parameter to append to
+ * @src: string parameter to append
+ *
+ * Return: strcat(dest, src)
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
