b7a1350d1107d1190406c77639a03b739cb2e443
MaleboGit <maleboinfo@gmail.com>
2023-07-03 11:56:12 -0700

    function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: character parameter to copy src string
+ * @src: string parameter to copy
+ * @n: integer parameter
+ *
+ * Return: strncpy(dest, src, n)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
