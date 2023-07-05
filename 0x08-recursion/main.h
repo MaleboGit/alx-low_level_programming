83b90d731d93a31fb01af364217a8acdca4bf31f
MaleboGit <maleboinfo@gmail.com>
2023-07-05 16:15:59 -0700

    function that returns the value of x raised to the power of y

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void _putchar(char x);
 
@@ -8,3 +9,4 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
