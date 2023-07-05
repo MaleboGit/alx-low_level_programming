b0bb9c370c66d27454e2a344e0919128c9669415
MaleboGit <maleboinfo@gmail.com>
2023-07-05 13:37:58 -0700

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: character parameter
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void _puts_recursion(char *s);
