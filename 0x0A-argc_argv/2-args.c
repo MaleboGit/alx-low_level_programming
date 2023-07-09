2d92dff27f573946da37245502e689934aafb4bf
MaleboGit <maleboinfo@gmail.com>
2023-07-08 22:58:29 -0700

    program that prints all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,13 +3,16 @@
 /**
  * main - Entry point
  *
+ * @argc: number of arguments (int parameter)
+ * @argv: string arguments (char parameter)
+ *
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <= argc -1; i++)
+	for (i = 0; i <= argc - 1; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
