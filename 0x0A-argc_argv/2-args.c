ff4ffcc634741d8d80deb735426ab4130f5fb38b
MaleboGit <maleboinfo@gmail.com>
2023-07-08 22:52:05 -0700

    program that prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc -1; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
