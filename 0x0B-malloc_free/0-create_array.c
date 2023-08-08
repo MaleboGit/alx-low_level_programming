65cc18405c982c3ea1294a605711f68c728e690f
MaleboGit <maleboinfo@gmail.com>
2023-08-08 11:13:55 -0700

    function that creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,19 @@
  *
  * @size: size of memory to allocate
  * @c: array char element
- * 
- * Return: Nothing.
+ *
+ * Return: NULL or memo
  */
+char *create_array(unsigned int size, char c)
+{
+	char *memo = malloc(sizeof(c) * size);
+
+	if (size == 0 || memo == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (memo);
+	}
+}
